372298dae1bad3669dd83141ed779e0433979376
Yury <chernyakov.yuriy@gmail.com>
2025-08-23 17:31:44 -0400

    Add mock users and update UserResolver with `getUserById` query

---
+++ src/__mocks__/mockUsers.ts
@@ -0,0 +1,17 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'user1',
+    displayName: 'Test User 1',
+  },
+  {
+    id: 2,
+    username: 'user2',
+    displayName: 'Test User 2',
+  },
+  {
+    id: 3,
+    username: 'user3',
+    displayName: 'Test User 3',
+  },
+];
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,14 +1,11 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { User } from '../models/User';
+import { mockUsers } from '../../__mocks__/mockUsers';
 
 @Resolver()
 export class UserResolver {
-  @Query((returns) => User)
-  getUser() {
-    return {
-      id: 1,
-      username: 'test',
-      displayName: 'Test User',
-    };
+  @Query((returns) => User, { nullable: true })
+  getUserById(@Args('id', { type: () => Int }) id: number) {
+    return mockUsers.find((user) => user.id === id);
   }
 }
